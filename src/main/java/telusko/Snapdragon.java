e9ec7e6a8b9aeecec4d0813e0bc0d723d14a0afd
Zhang Kaian <zhangkaian@gmail.com>
2019-09-03 20:05:30 +0800

    autowire without bean tag

--- src/main/java/telusko/Snapdragon.java
+++ src/main/java/telusko/Snapdragon.java
@@ -1,5 +1,8 @@
 package telusko;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class Snapdragon implements MobileProcessor{
 	public void process() {
 		System.out.println("Cpu");
