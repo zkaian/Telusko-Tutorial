2a096752f030a4cfb310deada19a66a20a71be1c
Zhang Kaian <zhangkaian@gmail.com>
2019-09-03 13:06:10 +0800

    Add project to git

---
+++ README.md
@@ -0,0 +1,1 @@
+Basic Spring tutorial from Telusko
---
+++ src/main/java/tacos/TeluskoTutorialApplication.java
@@ -0,0 +1,13 @@
+package tacos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeluskoTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeluskoTutorialApplication.class, args);
+	}
+
+}
